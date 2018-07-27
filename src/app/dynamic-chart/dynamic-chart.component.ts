305c65fff8a074ee71cc1cac120659cfee5f8d0d
Abdus Sattar K M <AK00507516@techmahindra.com>
2018-07-27 22:15:29 +0530

    Basic Examples

---
+++ src/app/dynamic-chart/dynamic-chart.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-chart',
+  templateUrl: './dynamic-chart.component.html',
+  styleUrls: ['./dynamic-chart.component.scss']
+})
+export class DynamicChartComponent implements OnInit {
+
+  // lineChart
+  public lineChartData: Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType: String = 'line';
+  public pieChartType: String = 'pie';
+
+  // Pie
+  public pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartData: number[] = [300, 500, 100];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public randomizeType(): void {
+    this.lineChartType = this.lineChartType === 'line' ? 'bar' : 'line';
+    this.pieChartType = this.pieChartType === 'doughnut' ? 'pie' : 'doughnut';
+  }
+
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+}
